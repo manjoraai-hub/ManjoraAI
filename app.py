b4b59aa5c49702dbba3fc33fbd83a39913ce15d2
manjoraai-hub <manjumanjeh@gmail.com>
2026-05-09 19:30:35 +0530

    professional landing page added

--- app.py
+++ app.py
@@ -2,39 +2,47 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Login page will open first
+# Landing page first
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return render_template('index.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
-
+# Login page
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+# Register page
 @app.route('/register')
 def register():
     return render_template('register.html')
 
+# Dashboard
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+# Planner
 @app.route('/planner')
 def planner():
     return render_template('planner.html')
 
+# Career Roadmaps
 @app.route('/career')
 def career():
     return render_template('career.html')
 
+# Reminder Page
 @app.route('/reminder')
 def reminder():
     return render_template('reminder.html')
 
+# Books & Lectures
 @app.route('/books')
 def books():
     return render_template('books.html')
 
+# Current Affairs
 @app.route('/affairs')
 def affairs():
     return render_template('affairs.html')
