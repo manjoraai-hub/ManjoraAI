9590d49ac3badedf764136dbe936bf9363aa188c
manjoraai-hub <manjumanjeh@gmail.com>
2026-05-09 18:09:10 +0530

    real ai connected

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+from openai import OpenAI
+import os
 
 app = Flask(__name__)
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
 
 @app.route('/')
 def home():
@@ -13,9 +19,36 @@ def ask_ai():
 
     user_message = data.get('message')
 
-    return jsonify({
-        "reply": f"You said: {user_message}"
-    })
+    try:
+
+        response = client.chat.completions.create(
+
+            model="gpt-3.5-turbo",
+
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are Manjora AI, a helpful AI mentor."
+                },
+                {
+                    "role": "user",
+                    "content": user_message
+                }
+            ]
+
+        )
+
+        ai_reply = response.choices[0].message.content
+
+        return jsonify({
+            "reply": ai_reply
+        })
+
+    except Exception as e:
+
+        return jsonify({
+            "reply": str(e)
+        })
 
 if __name__ == '__main__':
     app.run(debug=True)
